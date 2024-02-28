a5c67eb1035129cd4a6ebc353935ff0a03f3580f
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2024-02-28 10:51:43 -0300

    Creation of data folder to incorporate data.ts of projects with its interface brought from types/portfolio.ts. We also added images

---
+++ app/data/data.ts
@@ -0,0 +1,87 @@
+import {ProyectInterface} from '../types/portfolio'
+
+
+
+export const proyects: ProyectInterface[] = [
+
+    {
+        id: 1,
+        tittle:'JC PERSONAL TRAINER',
+        description:'Desarrollo de landing page Jc personal trainer con el objetivo de difundir el trabajo de un grupo de entrenadores de Córdoba Capital, creada con Next.js 14, Typescript y diseñada con Tailwind y Shadcn',
+        url:'https://jcpersonaltrainer.vercel.app/',
+        img:'/jcptrainer.png'      
+    },
+    {
+        id: 2,
+        tittle:'QUIEN JUEGA HOY',
+        description:'Agenda deportiva de fútbol exclusivamente, con el fin de informar sobre los partidos de hoy y mañana, con horario argentino. Utilicé Next js y Material UI, consumiendo API de fútbol',
+        url:'https://quienjuegahoy.vercel.app/',
+        img:'/quienjuegahoy.png'      
+    },
+
+    {
+        id: 3,
+        tittle:'RAIZ CARPINTERÍA DE DISEÑO',
+        description:'Desarrollo de aplicación web para empresa Raiz Carpintería de Diseño, utilizando tencologías como Next.js y Material UI con estilos propios para confeccionar la aplicación',
+        url:'https://raizcarpinteria.vercel.app/',
+        img:'/raizweb3b.png'      
+    },
+
+    {
+        id: 4,
+        tittle:'PORTFOLIO',
+        description:'Desarrollo y re-versión de Curriculum, transformándo el sitio web anterior en una app más potente con tecnologías avanzadas como JavaScript, React.js y Next.js',
+        url:'https://portfoliojcc.vercel.app/',
+        img:'/newportfolio.png',      
+    },
+    
+    {
+        id: 5,
+        tittle:'HARRY POTTER QUIZ',
+        description:"Quiz app de 23 preguntas con temática Harry Potter utilizando tencologías como JavaScript, React js, Next js y Firebase con el fin de guardar puntuación y datos del participante",
+        url:'https://harrypotterquizgame.vercel.app/',
+        img:'/hp2.png'      
+    },
+    
+    {
+        id: 6,
+        tittle:'SCALONETA QUIZ',
+        description:"Quiz app sobre Selección Argentina utilizando tencologías como JavaScript, React js y Firebase con la finalidad de almacenar nombre y puntuación del participante",
+        url:'https://scalonets.netlify.app/',
+        img:'/scaloneta.png'      
+    },
+    
+    {
+        id:7,
+        tittle:'MM CHOCOLATES APP',
+        description:'Creamos reversión de sitio web Mm Chocolates, transformándo el mismo en un e-commerce con tecnologías como JavaScript, React.js y Firebase almacenando la orden de compra hecha',
+        url:'https://mmchocolates.netlify.app/',
+        img:'/mmchocolates.png'      
+    },
+    
+    {
+        id: 8,
+        tittle:'NIKE STORE',
+        description:'Desarrollo de e-commerce Nike app con tecnologías avanzadas como JavaScript, React.js, Adobe Photoshop y Firebase con el fin de guardar la orden de compra realizada por el usuario',
+        url:'https://thenikestore.netlify.app/',
+        img:'/nike.png'      
+    },
+    
+    {
+        id: 9,
+        tittle:'MM CHOCOLATES SITIO WEB',
+        description:'Desarrollo y maquetación de sitio web de emprendimiento de chocolates, con tecnologías básicas como HTML, CSS, Bootstrap y JavaScript consumiendo API Email.js para enviar pedido',
+        url:'https://jcruzcorti.github.io/MMchocolates/',
+        img:'/mmchocolates.png'      
+    },
+    
+    {
+        id: 10,
+        tittle:'JUGADORES HISTÓRICOS',
+        description:'Proyecto de Blog futbolero sobre una selección de jugadores de fútbol favoritos históricos, actuales y futuros, desarrollado y maquetado con tecnologías básicas como HTML, CSS',
+        url:'https://jcruzcorti.github.io/JH/',
+        img:'/jh.png'      
+    },
+    
+
+]
---
+++ app/types/portfolio.ts
@@ -0,0 +1,7 @@
+export interface ProyectInterface {
+    id: number,
+    tittle: string,
+    description: string,
+    url: string,
+    img: string
+}
